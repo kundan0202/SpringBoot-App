fb913b475d17bb4986480935c01db5012d045c89
Kundan <kundan.kk0202@gmail.com>
2020-11-22 09:19:49 +0530

    Added Car class

---
+++ src/main/java/com/webproject/beans/Car.java
@@ -0,0 +1,10 @@
+package com.webproject.beans;
+
+public class Car {
+	
+	private void syso() {
+		// TODO Auto-generated method stub
+             System.out.println("Hello!Mecrcedes welcome you");
+	}
+
+}
