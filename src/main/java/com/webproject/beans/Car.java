c032f969ed79963e8f26ad395ba2f8bb779faabe
Kundan <kundan.kk0202@gmail.com>
2020-11-22 17:48:06 +0530

    HIS-134 Enhancement provided

--- src/main/java/com/webproject/beans/Car.java
+++ src/main/java/com/webproject/beans/Car.java
@@ -5,6 +5,9 @@ public class Car {
 	private void syso() {
 		// TODO Auto-generated method stub
              System.out.println("Hello!Mecrcedes welcome you");
+             
+				/* Chnages for HIS-134 */
+             System.out.println("Enhancement in code as per HIS-134");
 	}
 
 }
