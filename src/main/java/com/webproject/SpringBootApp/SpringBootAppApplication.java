9653e7f8e4192ca1ab4ac6221e5320b200114c9a
Kundan <kundan.kk0202@gmail.com>
2020-11-22 09:04:09 +0530

    Main class modified

--- src/main/java/com/webproject/SpringBootApp/SpringBootAppApplication.java
+++ src/main/java/com/webproject/SpringBootApp/SpringBootAppApplication.java
@@ -2,12 +2,14 @@ package com.webproject.SpringBootApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppApplication.class, args);
+		ConfigurableApplicationContext run=SpringApplication.run(SpringBootAppApplication.class, args);
+		run.close();
 	}
 
 }
