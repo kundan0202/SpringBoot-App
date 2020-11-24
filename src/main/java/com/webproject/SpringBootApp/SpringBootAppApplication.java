c3627fe29e663d1c354dc7ffd06c2ad6f61c4655
Kundan <kundan.kk0202@gmail.com>
2020-11-24 20:19:42 +0530

    HIS-301:Load cah method added

--- src/main/java/com/webproject/SpringBootApp/SpringBootAppApplication.java
+++ src/main/java/com/webproject/SpringBootApp/SpringBootAppApplication.java
@@ -7,12 +7,20 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class SpringBootAppApplication {
 
+	//Saurav data changes1
+	private Map<String,Object> cache= new HashMap<String,Object>()
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run=SpringApplication.run(SpringBootAppApplication.class, args);
 		run.close();
 		
 		//suman code chnages
 		int i=10;
+
+	//Saurav data changes2
+	private void loadCash(){
+		//LOGIC
+		}
 	}
 
 }
